b16c8b3dcdbdd6bb5cfd02bdc328b3e87dea140f
Prosper Shaibu Asare <asareprosper143@gmail.com>
2026-06-04 11:59:30 +0000

    feat: initialize axios instance with request and response interceptors for token management

--- mindbridge-mobile/src/services/api.ts
+++ mindbridge-mobile/src/services/api.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   // For Android Emulator use: http://10.0.2.2:5000/api
   // For iOS Simulator or Web use: http://localhost:5000/api
   // For Physical Device, use your computer's local IP (e.g., http://192.168.1.X:5000/api)
-  baseURL: 'http://192.168.43.223:5000/api', 
+  baseURL: 'http://172.20.10.3:5000/api', 
 });
 
 api.interceptors.request.use(async (config) => {
