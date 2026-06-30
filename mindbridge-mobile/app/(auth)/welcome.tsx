ae41aac71c2aa2c71b5747458a6539e79bd5c189
Prosper Shaibu Asare <asareprosper143@gmail.com>
2026-06-30 14:12:55 +0000

    feat: implement welcome screen with animated hero section and feature cards

--- mindbridge-mobile/app/(auth)/welcome.tsx
+++ mindbridge-mobile/app/(auth)/welcome.tsx
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { useTheme } from '../../src/context/ThemeContext';
 import { 
   ArrowRight, 
